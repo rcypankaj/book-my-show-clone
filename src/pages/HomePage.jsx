d0ece0e7d59da783ea1bc9a14e44383ab70925b6
Pankaj <rcypankaj@gmail.com>
2023-08-09 13:37:57 +0530

    added more featured carousel

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -51,7 +51,7 @@ const HomePage = () => {
   }, []);
   return (
     <Fragment>
-      {/* <HeroCarousel /> */}
+      <HeroCarousel />
       <div className="container mx-auto px-4 md:px-12 my-8">
         <h1 className="text-2xl font-bold text-gray-800 sm:ml-3 ml-0 my-3">
           The Best of Entertainment
@@ -66,7 +66,7 @@ const HomePage = () => {
           isDark={false}
         />
       </div>
-      {/*
+
       <div className="bg-premier-800 py-12">
         <div className="container mx-auto px-4 md:px-12 my-8 flex flex-col gap-3">
           <div className="hidden md:flex">
@@ -91,7 +91,7 @@ const HomePage = () => {
           posters={onlineStreamEvents}
           isDark={false}
         />
-      </div> */}
+      </div>
     </Fragment>
   );
 };
